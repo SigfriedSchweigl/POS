b68b4e9d9dcf89d6dac50cea269ca7f4c0ebf7e6
sigfriedschweigl <163283797+SigfriedSchweigl@users.noreply.github.com>
2026-04-01 09:08:17 +0000

    Simplify toggleSolution logic with class toggle
    Refactor toggleSolution function to use class toggle.

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 function toggleSolution(id) {
   const element = document.getElementById(id);
-
-  if (element.style.display === "block") {
-    element.style.display = "none";
-  } else {
-    element.style.display = "block";
-  }
-}
-
-function toggleSolution(id) {
-  const element = document.getElementById(id);
   element.classList.toggle("show");
 }
 
